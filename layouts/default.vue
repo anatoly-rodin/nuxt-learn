2a1748087e2e1c1803edcdf8b6e17c27ee819856
Anatolii Rodin <anatolyr1@gmail.com>
2020-05-06 19:35:44 +0300

    Edit layout template and add navbar component.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <nuxt />
+    <Navbar />
+    <main>
+      <div class="container">
+        <nuxt />
+      </div>
+    </main>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    Navbar
+  }
+}
+</script>
