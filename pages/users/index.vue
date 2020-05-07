bdad2371c3a288a8f6a4d70d61c01eaf82a6ab09
Anatolii Rodin <anatolyr1@gmail.com>
2020-05-07 12:53:36 +0300

    Create users component.

---
+++ pages/users/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>Users page</h1>
+    <ul>
+      <li
+        v-for="user of 5"
+        :key="user"
+      >
+        {{ user }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Users'
+}
+</script>
